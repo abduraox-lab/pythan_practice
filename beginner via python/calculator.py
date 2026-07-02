cf13022c6b5599d035ee2b0f5c1cbec960e916fa
abdurrahman4kj-bit <abdur.rahman4kj@gmail.com>
2026-07-02 16:21:39 +0530

    added calculator file

--- beginner via python/calculator.py
+++ beginner via python/calculator.py
@@ -1,1 +1,19 @@
-print("calculator")
+
+opreator = input("enter your opreator (+, -, *, /, ): ")
+num1 = float(input("enter your first number: "))
+num2 = float(input("enter your second number: "))
+
+if opreator == "+":
+    result = num1 + num2
+    print(result)
+elif opreator == "-":
+    result = num1 - num2
+    print(result)
+elif opreator == "*":
+    result = num1 * num2
+    print(result)
+elif opreator == "/":
+    result = num1 / num2
+    print(result)
+else:
+    print(f"{opreator} is not valid")
